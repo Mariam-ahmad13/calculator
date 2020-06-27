636139453c6628023e04d0ac68e366e4e2eaa998
Mariam-ahmad13 <mariamahmad10@outlook.com>
2020-06-27 21:29:32 +0300

    new

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-var input = document.getElementById('input')
-var add = document.getElementById('add')
+var input = document.getElementById("input")
+function getNumber(num) {
+         input.value += num;
+}
+
+function pi(){
+    input.value = input.value * 3.14
+}
+function clearResult(){
+         input.value = "";
+}
+
+function getResult(){
+     input.value = eval(input.value)
+}
+
+function square(){
+    input.value = input.value * input.value;
+}
+
+function cube(){
+    input.value = input.value * input.value * input.value;
+}
